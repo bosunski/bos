2817ded8b1f8cd2771e1844df71ca03d9aa0d243
Bosun Egberinde <bosunski@gmail.com>
2017-09-01 17:39:48 +0100

    RRR

--- js/my-app.js
+++ js/my-app.js
@@ -1,3 +1,5 @@
+
+
 // Initialize app
 var myApp = new Framework7();
 
@@ -14,40 +16,55 @@ var mainView = myApp.addView('.view-main', {
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
     console.log("Device is ready!");
+
+    $('.pic-icon').click(function() {
+      myApp.alert('Here comes Mail Page');
+    });
 });
 
 
-// Now we need to run the code that will be executed only for About page.
-
-// Option 1. Using page callback for page (for "about" page in this case) (recommended way):
-myApp.onPageInit('mail', function (page) {
-    // Do something here for "about" page
-    //myApp.alert('Here comes Mail Page');
-    
-})
-
-myApp.onPageInit('about', function (page) {
-    // Do something here for "about" page
-    //myApp.alert('Here comes Mail Page');
-})
-
-/* myApp.onPageInit('mail', function(page) {
-  myApp.alert('Do well to send me Message Anytime.');
-})
-
 // Option 2. Using one 'pageInit' event handler for all pages:
 $$(document).on('pageInit', function (e) {
+  $('#subsub').click(function() {
+      var to = 'bosunski@gmail.com'
+      var message = $('#message').val();
+      var subject = $('#subject').val();
+      $.myApp = myApp;
+
+      if(message == null || message == '' || subject == null || subject == '') {
+        myApp.alert('Please fill the provided spaces');
+        $('.modal-title').html('Bosunski\'s Contact');
+      } else {
+        var url = 'http://fund.dev';
+        var resp = '';
+        $.myApp = myApp;
+        $.ajax({
+          url: url,
+          method: 'GET',
+          data: 'to='+to+'&body='+message+'&subject='+subject,
+          headers: {
+                    'Access-Control-Allow-Origin': '*'
+                },
+          success: function(res) {
+            $('#error').html(res);
+          },
+          error: function(res) {
+            $('#error').html('Unable to send your message, make sure you have internet connection and try again.')
+          }
+        })
+      }
+  });
     // Get page data from event data
     var page = e.detail.page;
 
     if (page.name === 'about') {
         // Following code will be executed for page with data-page attribute equal to "about"
-        myApp.alert('Here comes About page');
+//myApp.alert(page.name);
     }
 })
 
 // Option 2. Using live 'pageInit' event handlers for each page
-$$(document).on('pageInit', '.page[data-page="about"]', function (e) {
+/*$$(document).on('pageInit', '.page[data-page="about"]', function (e) {
     // Following code will be executed for page with data-page attribute equal to "about"
     myApp.alert('Here comes About page');
 }) */
